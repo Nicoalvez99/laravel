6698fafced4c27c4df59ae6e5bfdacba29821520
Nicoalvez99 <nicoalvez99@gmail.com>
2023-05-23 19:49:17 -0300

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\MensajeController;
-use App\Http\Controllers\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,30 +14,18 @@ use App\Http\Controllers\ProjectController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Otra forma de realizar las vistas y mejor es:
-Route::view('/', 'home')->name('home');
-//donde el primer parametro de view es la url y  el segundo es la vista, donde el name es home.
-Route::view('/about', 'about')->name('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('portfolio', ProjectController::class)->parameters(['portfolio' => 'project'])->names('projects');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//Route::resource lo que hace es simplificar todo lo que tengo aca abajo que tenga como igual la url la primera parte del name y el controlador, de esa manera la indico los parámetros a reemplazar y se simplifica todo.
-
-/*Route::get('/portfolio', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/portfolio/crear', [ProjectController::class, 'create'])->name('projects.create');
-Route::post('/portfolio', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/portfolio/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::get('/portfolio/{id}', [ProjectController::class, 'show'])->name('projects.show');
-Route::patch('/portfolio/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('/portfolio/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');*/
-
-
-
-Route::post('contacto', [MensajeController::class, 'store']);
-Route::view('/contacto', 'contacto')->name('contacto');
-
-
-
-
-
+require __DIR__.'/auth.php';
